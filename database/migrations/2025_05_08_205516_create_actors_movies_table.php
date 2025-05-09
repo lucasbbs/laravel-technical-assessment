018ffeaab437d7fbd3575b55ab5b072e76a0c8d6
Lucas Braga <lucasbbs@live.fr>
2025-05-09 15:39:18 -0400

    Add migrations

---
+++ database/migrations/2025_05_08_205516_create_actors_movies_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('actors_movies', function (Blueprint $table) {
+            $table->foreignId('actor_id')->constrained()->onDelete('cascade');
+            $table->foreignId('movie_id')->constrained()->onDelete('cascade');
+            $table->primary(['actor_id', 'movie_id']);
+            $table->timestamps();});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('actors_movies');
+    }
+};
